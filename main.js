cd3c566e5ca0bcd1bfd7b7e808074602e53bd614
Nabil Hadi <napilfadl@gmail.com>
2022-01-19 16:25:39 +0300

    Added typescript and eslint-typescript

--- main.js
+++ main.js
@@ -1,66 +1,84 @@
-const Discord = require("discord.js");
-require("dotenv").config();
-
-const generateIamge = require("./generateImage");
-
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = __importStar(require("discord.js"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const fs_1 = __importDefault(require("fs"));
+dotenv_1.default.config();
 const prefix = "rc!";
-
-const fs = require("fs");
 const commandDirPath = "./commands/";
-
-const client = new Discord.Client({
-  intents: [
-    "GUILDS",
-    "GUILD_MESSAGES",
-    "GUILD_MEMBERS",
-    "GUILD_VOICE_STATES",
-  ],
+const client = new discord_js_1.default.Client({
+    intents: [
+        discord_js_1.Intents.FLAGS.GUILDS,
+        discord_js_1.Intents.FLAGS.GUILD_MESSAGES,
+        discord_js_1.Intents.FLAGS.GUILD_MEMBERS,
+        discord_js_1.Intents.FLAGS.GUILD_VOICE_STATES,
+    ],
 });
-
-
-client.commands = new Discord.Collection();
-
-const commandFilesNames = fs.readdirSync(commandDirPath).filter(file => file.endsWith(".js"));
+const commands = new discord_js_1.default.Collection();
+const commandFilesNames = fs_1.default
+    .readdirSync(commandDirPath)
+    .filter((file) => file.endsWith(".js"));
 for (const fileName of commandFilesNames) {
-  const commandFile = require(`${commandDirPath}${fileName}`);
-  client.commands.set(commandFile.name, commandFile);
+    const commandFile = require(`${commandDirPath}${fileName}`);
+    commands.set(commandFile.name, commandFile);
 }
-
 client.on("ready", () => {
-  console.log(`Loggen in as ${client.user.tag}`);
+    console.log("Bot is ready!");
 });
-
-
 client.on("messageCreate", (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  const args = message.content.slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  if (command === "ping") {
-    client.commands.get("ping").execute(message);
-  } else if (command === "play") {
-    client.commands.get("play").execute(message, args);
-  } else if (command === "leave") {
-    client.commands.get("leave").execute(message);
-  } else if (command === "skip") {
-    client.commands.get("skip").execute(message);
-  } else if (command === "pause") {
-    client.commands.get("pause").execute(message);
-  } else if (command === "unpause") {
-    client.commands.get("unpause").execute(message);
-  }
+    var _a;
+    if (!message.content.startsWith(prefix) || message.author.bot)
+        return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = (_a = args === null || args === void 0 ? void 0 : args.shift()) === null || _a === void 0 ? void 0 : _a.toLowerCase();
+    if (command === "ping") {
+        commands.get("ping").execute(message);
+    }
+    else if (command === "play") {
+        commands.get("play").execute(message, args);
+    }
+    else if (command === "leave") {
+        commands.get("leave").execute(message);
+    }
+    else if (command === "skip") {
+        commands.get("skip").execute(message);
+    }
+    else if (command === "pause") {
+        commands.get("pause").execute(message);
+    }
+    else if (command === "unpause") {
+        commands.get("unpause").execute(message);
+    }
 });
-
-const welcomeChannelId = "531492104309047309";
-
-client.on("guildMemberAdd", async (member) => {
-  const img = await generateIamge(member);
-  member.guild.channels.cache.get(welcomeChannelId).send({
-    content: `<@${member.id}> Welcome to the server!`,
-    files: [img],
-  });
-});
-
-
+// const generateIamge = require("./generateImage");
+// const welcomeChannelId = "531492104309047309";
+// client.on("guildMemberAdd", async (member) => {
+// 	const img = await generateIamge(member);
+// 	member?.guild?.channels?.cache?.get(welcomeChannelId)?.send({
+// 		content: `<@${member.id}> Welcome to the server!`,
+// 		files: [img],
+// 	});
+// });
 client.login(process.env.TOKEN);
---
+++ main.ts
@@ -0,0 +1,65 @@
+import DiscordJS, { Intents } from "discord.js";
+import dotenv from "dotenv";
+import fs from "fs";
+dotenv.config();
+
+const prefix = "rc!";
+const commandDirPath = "./commands/";
+
+const client = new DiscordJS.Client({
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_VOICE_STATES,
+	],
+});
+
+const commands = new DiscordJS.Collection<string, any>();
+
+const commandFilesNames = fs
+	.readdirSync(commandDirPath)
+	.filter((file) => file.endsWith(".js"));
+for (const fileName of commandFilesNames) {
+	const commandFile = require(`${commandDirPath}${fileName}`);
+	commands.set(commandFile.name, commandFile);
+}
+
+client.on("ready", () => {
+	console.log("Bot is ready!");
+});
+
+client.on("messageCreate", (message) => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args?.shift()?.toLowerCase();
+
+	if (command === "ping") {
+		commands.get("ping").execute(message);
+	} else if (command === "play") {
+		commands.get("play").execute(message, args);
+	} else if (command === "leave") {
+		commands.get("leave").execute(message);
+	} else if (command === "skip") {
+		commands.get("skip").execute(message);
+	} else if (command === "pause") {
+		commands.get("pause").execute(message);
+	} else if (command === "unpause") {
+		commands.get("unpause").execute(message);
+	}
+});
+
+// const generateIamge = require("./generateImage");
+
+// const welcomeChannelId = "531492104309047309";
+
+// client.on("guildMemberAdd", async (member) => {
+// 	const img = await generateIamge(member);
+// 	member?.guild?.channels?.cache?.get(welcomeChannelId)?.send({
+// 		content: `<@${member.id}> Welcome to the server!`,
+// 		files: [img],
+// 	});
+// });
+
+client.login(process.env.TOKEN);
