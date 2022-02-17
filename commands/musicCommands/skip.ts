c2ec197026d565ec0cdfef77de91c315f06fa5a7
Nabil Hadi <napilfadl@gmail.com>
2022-02-17 23:26:15 +0300

    fix bugs

--- commands/musicCommands/skip.ts
+++ commands/musicCommands/skip.ts
@@ -1,23 +1,26 @@
 import { ICommand } from "wokcommands";
-import { skipTrack } from "../../utils/musicUtils/musicPlayer";
+import {
+  checkMusicPermission,
+  skipTrack,
+} from "../../utils/musicUtils/musicPlayer";
 export default {
-	category: "Music",
-	description: "skip currently playing song", // Required for slash commands
+  category: "Music",
+  description: "skip currently playing song", // Required for slash commands
 
-	guildOnly: true,
-	slash: false, // Create both a slash and legacy command
-	testOnly: false, // Only register a slash command for the testing guilds
+  guildOnly: true,
+  slash: false, // Create both a slash and legacy command
+  testOnly: false, // Only register a slash command for the testing guilds
 
-	callback: ({ message, guild, member }) => {
-		if (guild === null) return;
+  callback: ({ message, guild, member }) => {
+    if (guild === null) return;
 
-		const permission = checkMusicPermission(member, true);
-		if (permission.hasPermission === false) {
-			message.reply(permission.denyReason.description);
-			return;
-		}
+    const permission = checkMusicPermission(member, true);
+    if (permission.hasPermission === false) {
+      message.reply(permission.denyReason.description);
+      return;
+    }
 
-		skipTrack(guild.id);
-		// TODO: handle slash command interaction
-	},
+    skipTrack(guild.id);
+    // TODO: handle slash command interaction
+  },
 } as ICommand;
--- commands/musicCommands/unpause.ts
+++ commands/musicCommands/unpause.ts
@@ -1,45 +1,42 @@
 import { AudioPlayerStatus } from "@discordjs/voice";
 import { ICommand } from "wokcommands";
-import {
-	checkMusicPermission,
-	getAudioPlayerStatus,
-} from "../../utils/musicUtils/musicPlayer";
+import { checkMusicPermission } from "../../utils/musicUtils/musicPlayer";
 import { unpausePlayer } from "../../utils/musicUtils/musicPlayer";
 
 export default {
-	category: "Music",
-	description: "unpause music player", // Required for slash commands
+  category: "Music",
+  description: "unpause music player", // Required for slash commands
 
-	guildOnly: true,
-	slash: false, // Create both a slash and legacy command
-	testOnly: false, // Only register a slash command for the testing guilds
+  guildOnly: true,
+  slash: false, // Create both a slash and legacy command
+  testOnly: false, // Only register a slash command for the testing guilds
 
-	callback: ({ message, guild, member }) => {
-		if (guild === null) return;
+  callback: ({ message, guild, member }) => {
+    if (guild === null) return;
 
-		const permission = checkMusicPermission(member, true);
+    const permission = checkMusicPermission(member, true);
 
-		if (permission.hasPermission === false) {
-			message.reply(permission.denyReason.description);
-			return;
-		}
+    if (permission.hasPermission === false) {
+      message.reply(permission.denyReason.description);
+      return;
+    }
 
-		// if (isPlaying(guild.id) === true) {
-		// 	message.reply("Music is already playing!");
-		// 	return;
-		// }
+    // if (isPlaying(guild.id) === true) {
+    // 	message.reply("Music is already playing!");
+    // 	return;
+    // }
 
-		// if (getAudioPlayerStatus(guild.id) !== AudioPlayerStatus.Paused) {
-		// 	message.reply("Nothing there to unpause!");
-		// 	return;
-		// }
+    // if (getAudioPlayerStatus(guild.id) !== AudioPlayerStatus.Paused) {
+    // 	message.reply("Nothing there to unpause!");
+    // 	return;
+    // }
 
-		if (unpausePlayer(guild.id)) {
-			message.reply("unPaused");
-		} else {
-			message.reply("Did not unpause");
-		}
+    if (unpausePlayer(guild.id)) {
+      message.reply("unPaused");
+    } else {
+      message.reply("Did not unpause");
+    }
 
-		// TODO: handle slash command interaction
-	},
+    // TODO: handle slash command interaction
+  },
 } as ICommand;
