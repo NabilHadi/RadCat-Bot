04bad7561264b7c7fd35f413481fa56bfe8fcaf1
Nabil Hadi <napilfadl@gmail.com>
2022-06-09 23:23:02 +0300

    change status command

--- commands/configuration/status.ts
+++ commands/configuration/status.ts
@@ -1,41 +1,61 @@
-import { Client } from "discord.js";
+import { Client, ExcludeEnum } from "discord.js";
 import { ActivityTypes } from "discord.js/typings/enums";
 import { ICommand } from "wokcommands";
 
-const setStatus = (client: Client, status: string) => {
-	client.user?.setPresence({
-		status: "online",
+const getActivtyType = (text: String) => {
+  switch (text) {
+    case "listening":
+      return ActivityTypes.LISTENING;
+    case "watching":
+      return ActivityTypes.WATCHING;
+    case "streaming":
+      return ActivityTypes.STREAMING;
+    case "competing":
+      return ActivityTypes.COMPETING;
+    default:
+      return ActivityTypes.PLAYING;
+  }
+};
 
-		activities: [
-			{
-				name: status,
-				type: ActivityTypes.WATCHING,
-			},
-		],
-	});
+const setStatus = (
+  client: Client,
+  activityType: ExcludeEnum<typeof ActivityTypes, "CUSTOM">,
+  status: string
+) => {
+  client.user?.setPresence({
+    status: "online",
+    afk: true,
+
+    activities: [
+      {
+        name: status,
+        type: activityType,
+      },
+    ],
+  });
 };
 
 export default {
-	category: "Configuration",
-	description: "Updates the status for the bot",
+  category: "Configuration",
+  description: "Updates the status for the bot",
 
-	minArgs: 1,
-	expectedArgs: "<status>",
+  minArgs: 2,
+  expectedArgs: "<activity Type> <status>",
 
-	ownerOnly: true,
-	hidden: true,
+  ownerOnly: true,
+  hidden: true,
 
-	// This method is invoked only once whenever the command is registered
-	init: (client: Client) => {
-		const status = "Youtube";
-		setStatus(client, status);
-	},
+  // This method is invoked only once whenever the command is registered
+  init: (client: Client) => {
+    const status = "Youtube";
+    setStatus(client, "WATCHING", status);
+  },
 
-	callback: ({ client, text, message }) => {
-		setStatus(client, text);
+  callback: ({ client, args, message }) => {
+    setStatus(client, getActivtyType(args[0]), args[1]);
 
-		message.reply({
-			content: "Status set!",
-		});
-	},
+    message.reply({
+      content: "Status set!",
+    });
+  },
 } as ICommand;
